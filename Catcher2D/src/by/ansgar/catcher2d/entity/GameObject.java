7aa6cb574af37fcb5825e3fecc672025fcf181ab
Mensky <bv_1514@mail.ru>
2015-08-20 18:33:30 +0300

    Modified

--- Catcher2D/src/by/ansgar/catcher2d/entity/GameObject.java
+++ Catcher2D/src/by/ansgar/catcher2d/entity/GameObject.java
@@ -3,13 +3,23 @@ package by.ansgar.catcher2d.entity;
 import org.newdawn.slick.opengl.Texture;
 
 public interface GameObject {
-	
-//	public void update(int dx, int dy);
-	
+
+	// public void update(int dx, int dy);
+
 	public void draw();
-	
+
 	public void moving();
-	
+
 	public Texture loadTexture(String key);
-	
+
+	public boolean intersects(GameObject entity);
+
+	public double getX();
+
+	public double getY();
+
+	public void setX(double x);
+
+	public void setY(double y);
+
 }
